07052d22dec547ad3a612750fac90383eba9285d
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-08-09 19:03:06 -0400

    first commit

---
+++ Controllers/EmployerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Models;
+using TechJobsPersistent.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechJobsPersistent.Controllers
+{
+    public class EmployerController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessAddEmployerForm()
+        {
+            return View();
+        }
+
+        public IActionResult About(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TechJobsPersistent.Models;
+using TechJobsPersistent.ViewModels;
+using TechJobsPersistent.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechJobsPersistent.Controllers
+{
+    public class HomeController : Controller
+    {
+        private JobDbContext context;
+
+        public HomeController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Job> jobs = context.Jobs.Include(j => j.Employer).ToList();
+
+            return View(jobs);
+        }
+
+        [HttpGet("/Add")]
+        public IActionResult AddJob()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessAddJobForm()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Job theJob = context.Jobs
+                .Include(j => j.Employer)
+                .Single(j => j.Id == id);
+
+            List<JobSkill> jobSkills = context.JobSkills
+                .Where(js => js.JobId == id)
+                .Include(js => js.Skill)
+                .ToList();
+
+            JobDetailViewModel viewModel = new JobDetailViewModel(theJob, jobSkills);
+            return View(viewModel);
+        }
+    }
+}
