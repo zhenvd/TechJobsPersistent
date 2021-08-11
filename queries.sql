61bfe05eef22e43ecc3fa2eaf7bd2efad4421065
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-08-11 09:59:17 -0400

    sql queries

--- queries.sql
+++ queries.sql
@@ -3,4 +3,6 @@ Jobs.EmployerId = int | Jobs.Id = int | Jobs.Name = longtext
 --Part 2
 SELECT Name FROM Employers WHERE Location = "St. Louis City";
 --Part 3
-
+SELECT * FROM Skills Left JOIN JobSkills ON Skills.ID = JobSkills.SkillId
+WHERE JobSkills.JobId IS NOT NULL 
+ORDER BY name DESC;
